fbbdd2640dddafdba80332576d0c62d2831c9513
kyushun <27068326+kyushun@users.noreply.github.com>
2018-11-17 15:08:04 +0900

    add myconsole functions

--- routes/lib/train-delays.js
+++ routes/lib/train-delays.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const req = require("request");
 
+const API_NAME = 'Train_Delays_API';
 const FILE_PATH = './storage/data/train-delays/delays.json';
 const URL = 'https://rti-giken.jp/fhc/api/train_tetsudo/delay.json';
 
@@ -12,16 +13,17 @@ exports.update = (callback) => {
     };
     req.get(options, (err, res, json) => {
         if (err) {
-            console.log(err);
+            myconsole.log(myconsole.subjects.error, API_NAME, `An error has occurred - "${err}"`);
             return;
         }
 
         fs.writeFile(FILE_PATH,  JSON.stringify(json, null, "\t"), (err) => {
             if (err) {
-                console.log(err);
+                myconsole.log(myconsole.subjects.error, API_NAME, `An error has occurred - "${err}"`);
                 return;
             }
 
+            myconsole.log(myconsole.subjects.info, API_NAME, `Train delays data is updated`);
             if (typeof(callback) == 'function') {
                 callback(json, FILE_PATH);
             }
