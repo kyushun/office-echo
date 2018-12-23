f3673a2887e5a20914d227b05638a2d5f38772c8
kyushun <16fi046@ms.dendai.ac.jp>
2018-12-23 16:31:44 +0900

    add functions of notifications on header

--- resources/js/Header.jsx
+++ resources/js/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import kyureki from '../qreki';
+import { observer } from 'mobx-react';
 
-const ANIMATION_INTERVAL_SEC = 4;
+const ANIMATION_INTERVAL_SEC = 10;
 
 export default class Header extends React.Component {
     constructor(props) {
@@ -19,7 +20,6 @@ export default class Header extends React.Component {
             sec: ''
         };
         this._date = null;
-        this.animationCount = 0;
     }
 
     componentDidMount() {
@@ -28,46 +28,6 @@ export default class Header extends React.Component {
         this.clockInterval = setInterval(() => {
             this.updateClock();
         }, 1000);
-
-        this.cnt = 0;
-        setInterval(() => {
-            this.animation();
-            console.log('a');
-        }, ANIMATION_INTERVAL_SEC * 1000);
-    }
-
-    animation() {
-        const titleId = 'header-info-title';
-        const contentId = 'header-info-content';
-        const animTag = '-anim-';
-        const titleParent = document.getElementById(titleId);
-        const contentParent = document.getElementById(contentId);
-
-        var tc = titleParent.children[this.animationCount];
-        var tn = titleParent.children[this.animationCount + 1];
-        var cc = contentParent.children[this.animationCount];
-        var cn = contentParent.children[this.animationCount + 1];
-
-        if (typeof cn !== "undefined") {
-            this.animationCount++;
-        } else {
-            this.animationCount = 0;
-            tn = titleParent.children[0];
-            cn = contentParent.children[0];
-        }
-
-        if (typeof tc !== "undefined") {
-            tc.classList.remove(titleId + animTag + 'in');
-            tc.classList.add(titleId + animTag + 'out');
-        }
-        if (typeof cc !== "undefined") {
-            cc.classList.remove(contentId + animTag + 'in');
-            cc.classList.add(contentId + animTag + 'out');
-        }
-        tn.classList.remove(titleId + animTag + 'out');
-        tn.classList.add(titleId + animTag + 'in');
-        cn.classList.remove(contentId + animTag + 'out');
-        cn.classList.add(contentId + animTag + 'in');
     }
 
     getRokuyo() {
@@ -101,7 +61,6 @@ export default class Header extends React.Component {
                 date: date,
                 day: day
             },
-            //date: `${year}年${month + 1}月${date}日（${day}）`,
             time: `${hour}:${min}`,
             sec: `:${sec}`
         });
@@ -124,23 +83,190 @@ export default class Header extends React.Component {
                         {this.state.time}<div className="h-sec">{this.state.sec}</div>
                     </div>
                 </div>
-                <div className="header-info">
-                    <div className="header-info-frame">
-                        <div id="header-info-title" className="header-info-title-wrapper">
-                            <div id="t1" className="header-info-title header-info-title-anim-in">お知らせ</div>
-                            <div id="t2" className="header-info-title header-info-title-anim-out">天気</div>
-                            <div id="t2" className="header-info-title header-info-title-anim-out">鉄道遅延</div>
-                            <div id="t2" className="header-info-title header-info-title-anim-out">お知らせ</div>
-                        </div>
-                        <div id="header-info-content" className="header-info-content-wrapper">
-                            <div id="c1" className="header-info-content header-info-content-anim-in">ここはお知らせほんぶんです</div>
-                            <div id="c2" className="header-info-content header-info-content-anim-out">てんきをお知らせします</div>
-                            <div id="c2" className="header-info-content header-info-content-anim-out">JR山手線で遅延が発生しています</div>
-                            <div id="c2" className="header-info-content header-info-content-anim-out">JR山手線・地下鉄日比谷線・東武スカイツリーラインで遅延が発生しています</div>
-                        </div>
+                <Notification trainDelaysStore={this.props.trainDelaysStore} weatherStore={this.props.weatherStore} />
+            </div>
+        );
+    }
+}
+
+const Notification = observer(class Notification extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notifications: []
+        };
+        this.animationCount = 0;
+    }
+
+    componentDidMount() {
+        this.socket = io('/notification');
+        this.socket.on('disconnect', (err) => {
+            this.setState({
+                notifications: []
+            });
+        });
+        this.socket.on('notice', (data) => {
+            if (data && data.length > 0) {
+                this.setState({
+                    notifications: data
+                });
+            }
+        });
+
+        setInterval(() => {
+            this.animation();
+        }, ANIMATION_INTERVAL_SEC * 1000);
+    }
+
+    animation() {
+        const titleId = 'header-info-title';
+        const contentId = 'header-info-content';
+        const animTag = '-anim-';
+        const titleParent = document.getElementById(titleId);
+        const contentParent = document.getElementById(contentId);
+
+        var tc = titleParent.children[this.animationCount];
+        var tn = titleParent.children[this.animationCount + 1];
+        var cc = contentParent.children[this.animationCount];
+        var cn = contentParent.children[this.animationCount + 1];
+
+        if (typeof cn !== "undefined") {
+            this.animationCount++;
+        } else {
+            this.animationCount = 0;
+            tn = titleParent.children[0];
+            cn = contentParent.children[0];
+        }
+
+        if (typeof tc !== "undefined") {
+            tc.classList.remove(titleId + animTag + 'in');
+            tc.classList.add(titleId + animTag + 'out');
+        }
+        if (typeof cc !== "undefined") {
+            cc.classList.remove(contentId + animTag + 'in');
+            cc.classList.add(contentId + animTag + 'out');
+        }
+
+        if (typeof tn !== "undefined") {
+            tn.classList.remove(titleId + animTag + 'out');
+            tn.classList.add(titleId + animTag + 'in');
+        }
+        if (typeof cn !== "undefined") {
+            cn.classList.remove(contentId + animTag + 'out');
+            cn.classList.add(contentId + animTag + 'in');
+        }
+    }
+
+    render() {
+        const { currently, hourly, daily } = this.props.weatherStore;
+        const delays = this.props.trainDelaysStore.delays;
+
+        return (
+            <div className="header-info">
+                <div className="header-info-frame">
+                    <div id="header-info-title" className="header-info-title-wrapper">
+                        {(() => {
+                            if (currently.length > 0) {
+                                return (
+                                    <div className="header-info-title header-info-title-anim-out">現在の天気</div>
+                                );
+                            }
+                        })()}
+                        {(() => {
+                            if (hourly.length > 0) {
+                                return (
+                                    <div className="header-info-title header-info-title-anim-out">今後の天気</div>
+                                );
+                            }
+                        })()}
+                        {(() => {
+                            if (daily.length > 0) {
+                                return (
+                                    <div className="header-info-title header-info-title-anim-out">一週間の天気</div>
+                                );
+                            }
+                        })()}
+                        {(() => {
+                            if (delays.length > 0) {
+                                return (
+                                    <div className="header-info-title header-info-title-anim-out">鉄道遅延</div>
+                                );
+                            }
+                        })()}
+                        {this.state.notifications.map((_n) => {
+                            return (
+                                <div key={_n.title + _n.content} className="header-info-title header-info-title-anim-out">{_n.title}</div>
+                            );
+                        })}
+                    </div>
+                    <div id="header-info-content" className="header-info-content-wrapper">
+                        {(() => {
+                            if (currently.length > 0) {
+                                return (
+                                    <div className="header-info-content header-info-content-anim-out">
+                                        <span className="three-quarters-size">{currently[0].summary}</span>&nbsp;
+                                        <i className={`icon wi wi-forecast-io-${currently[0].icon}`} />&nbsp;&nbsp;
+                                        <span className="three-quarters-size">気温&nbsp;</span>{Math.round(currently[0].temperature)}&deg;C&nbsp;&nbsp;
+                                        <span className="three-quarters-size">降水確率&nbsp;</span>{Math.round(currently[0].precipProbability * 100)}%&nbsp;&nbsp;
+                                        <span className="three-quarters-size">湿度&nbsp;</span>{Math.round(currently[0].humidity * 100)}%&nbsp;&nbsp;
+                                        <span className="three-quarters-size">気圧&nbsp;</span>{Math.round(currently[0].pressure)}hPa
+                                    </div>
+                                );
+                            }
+                        })()}
+                        {(() => {
+                            if (hourly.length > 0) {
+                                return (
+                                    <div className="header-info-content header-info-content-anim-out">
+                                        3<span className="three-quarters-size">時間後</span>&nbsp;<i className={`icon wi wi-forecast-io-${hourly[4].icon}`} />&nbsp;&nbsp;
+                                        6<span className="three-quarters-size">時間後</span>&nbsp;<i className={`icon wi wi-forecast-io-${hourly[7].icon}`} />&nbsp;&nbsp;
+                                        9<span className="three-quarters-size">時間後</span>&nbsp;<i className={`icon wi wi-forecast-io-${hourly[10].icon}`} />&nbsp;&nbsp;
+                                        12<span className="three-quarters-size">時間後</span>&nbsp;<i className={`icon wi wi-forecast-io-${hourly[13].icon}`} />
+                                    </div>
+                                );
+                            }
+                        })()}
+                        {(() => {
+                            if (daily.length > 0) {
+                                return (
+                                    <div className="header-info-content header-info-content-anim-out">
+                                        {[...Array(4)].map((val, i) => {
+                                            const index = i + 1;
+                                            return (
+                                                <span key={daily[index].time}>
+                                                    {moment.unix(daily[index].time).format('MM/DD')}&nbsp;
+                                                <i className={`icon wi wi-forecast-io-${daily[index].icon}`} />
+                                                    <span className="three-quarters-size">(
+                                                    <span className="material-red">{Math.round(daily[index].temperatureHigh)}</span>/
+                                                    <span className="material-blue">{Math.round(daily[index].temperatureLow)}</span>
+                                                        )</span>&nbsp;&nbsp;
+                                            </span>
+                                            );
+                                        })}
+                                    </div>
+                                );
+                            }
+                        })()}
+                        {(() => {
+                            if (delays.length > 0) {
+                                return (
+                                    <div className="header-info-content header-info-content-anim-out">
+                                        {delays.map((line, i) => {
+                                            return (i != 0 ? '・' : '') + line.name;
+                                        })}
+                                        <span className="three-quarters-size">で遅延が発生しています</span>
+                                    </div>
+                                );
+                            }
+                        })()}
+                        {this.state.notifications.map((_n) => {
+                            return (
+                                <div key={_n.content} className="header-info-content header-info-content-anim-out">{_n.content}</div>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
         );
     }
-}
+});
